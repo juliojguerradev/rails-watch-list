a3f632cd93cdbb343aae715dab6a197110eac90a
Julio Guerra <juliowar33@gmail.com>
2021-11-09 20:50:23 -0500

    Add image upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+
   resources :lists, only: %i[show index new create] do
     resources :bookmarks, only: %i[show index new create destroy]
     resources :movies, only: %i[show index new create destroy]
